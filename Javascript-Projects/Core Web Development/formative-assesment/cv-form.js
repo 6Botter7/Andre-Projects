8bfdf414f37a49a7f213d448e98b77f13d7fa9a9
6Botter7 <7williamsandre7@gmail.com>
2022-08-18 22:40:42 +0200

    JS added to component
    JS added to component
    Css Clean Up

--- Javascript-Projects/Core Web Development/formative-assesment/cv-form.js
+++ Javascript-Projects/Core Web Development/formative-assesment/cv-form.js
@@ -1,26 +1,47 @@
 export class CvForm extends HTMLElement {
-    
+
     async connectedCallback() {
         this.innerHTML = await fetch(import.meta.url.replace(".js", ".html")).then(result => result.text());
-        console.log("Connected");
 
+        this.mouseClickHandler = this.mouseClick.bind(this);
+
+        this.addEventListener("click", this.mouseClickHandler);
 
         requestAnimationFrame(() => {
-            console.log("Connected 2");
+
+        })
+
+    }
+
+    async disconnectedCallback() {
+        this.removeEventListener("click", this.mouseDownHandler);
+        this.mouseDownHandler = null;
+    }
+
+    async mouseClick(event) {
+         
+        // console.log(this.querySelector(".app-form-control").value)
+        // console.log(event.target)
+        if (event.target.matches(".app-form-button")) {
+            const fullName = this.querySelector("#fullName").value;
+            const email = this.querySelector("#email").value;
+            const contactNumber = this.querySelector("#contactNumber").value;
+            const message = this.querySelector("#message").value;
+            const details = `Your name: ${fullName}, email: ${email}, contact number: ${contactNumber} and your messsage: ${message}`
+            alert("Your form has been 'submitted'" + details)
+
+            // const x = 200;
+            // const y = 100;
+            // // let position = event.target
+            // event.target.style.transform = `translate( ${x}px, ${y}px) scale(2) rotate(360deg)`;
+            // event.target.style.background = "#0276C2";
         }
-        )
     }
 
 
-    disconnectedCallback() {
-
-
-    }
-    
-    
- }
+}
 
 // cv1 = new Cv1();
 
 
-customElements.define("cv-form" , CvForm);
+customElements.define("cv-form", CvForm);
