f55101cef60564d432cf42e7c60c3006085c8aaa
6Botter7 <7williamsandre7@gmail.com>
2022-08-18 23:13:12 +0200

    Clean Up
    Removed Some code redundancy
    removed unnecessary comments
    added alert on js

--- Javascript-Projects/Core Web Development/formative-assesment/cv-form.js
+++ Javascript-Projects/Core Web Development/formative-assesment/cv-form.js
@@ -19,29 +19,21 @@ export class CvForm extends HTMLElement {
     }
 
     async mouseClick(event) {
-         
-        // console.log(this.querySelector(".app-form-control").value)
-        // console.log(event.target)
-        if (event.target.matches(".app-form-button")) {
+        if (event.target.matches("#sendBtn")) {
             const fullName = this.querySelector("#fullName").value;
             const email = this.querySelector("#email").value;
             const contactNumber = this.querySelector("#contactNumber").value;
             const message = this.querySelector("#message").value;
             const details = `Your name: ${fullName}, email: ${email}, contact number: ${contactNumber} and your messsage: ${message}`
             alert("Your form has been 'submitted'" + details)
+        }
 
-            // const x = 200;
-            // const y = 100;
-            // // let position = event.target
-            // event.target.style.transform = `translate( ${x}px, ${y}px) scale(2) rotate(360deg)`;
-            // event.target.style.background = "#0276C2";
+        if (event.target.matches("#cancelBtn")) {
+            alert("You Cancelled the form!")
         }
     }
 
 
 }
-
-// cv1 = new Cv1();
-
 
 customElements.define("cv-form", CvForm);
